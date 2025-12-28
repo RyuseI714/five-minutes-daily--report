db4c1deb1c64c3d28eaf82a8a048ba7a7e89b471
RyuseI714 <ryuusaki0714@gmail.com>
2025-12-28 10:41:47 +0900

    Fix typo: reprotId → reportId

--- src/app/reports/[reportId]/page.tsx
+++ src/app/reports/[reportId]/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import DeleteButton from "../DeleteButton"
 import { createClient } from "@/lib/supabase/server"
 import Link from "next/link"
